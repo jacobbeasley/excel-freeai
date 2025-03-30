d6197ce12fa2dda37df091944cf669bfabc30fa6
Jacob Beasley <jacobbeasley@gmail.com>
2025-03-30 00:25:27 -0500

    more utility functions, dark mode fix

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -3,17 +3,78 @@
 import OpenAI from "openai";
 
 /**
- * Add two numbers
+ * Utility function for string templating
  * @customfunction
- * @param {number} first First number
- * @param {number} second Second number
- * @returns {number} The sum of the two numbers.
-
-export function add(first, second) {
-  console.log("TEST");
-  return first + second;
-}
+ * @param {string[]} strings A list of values. The first one is the template and subsequent ones are the parameters to plug into the template. Performs like sprintf in C.
+ * @returns {string} The templated string.
  */
+export function format(strings) {
+  console.log(strings);
+  console.log(arguments);
+  //return "TEST";
+  try {
+    var s = strings[0];
+    for (var i = 0; i < strings.length - 1; i++) {
+    var reg = new RegExp("\\{" + i + "\\}", "gm");
+      s = s.replace(reg, strings[i + 1]);
+    }
+    return s;
+  } catch (e) {
+    console.log(e);
+    return "ERROR: " + e;
+  }
+}
+
+/**
+ * Utility function for finding things in strings with regex
+ * @customfunction
+ * @param {string} input to search
+ * @param {string} regex pattern to match.
+ * @param {string} [options] such as i for case insensitive.
+ * @returns {string} The matched pattern or error if not found.
+ */
+export function regexsearch(input, regex, options) {
+  console.log(arguments);
+  try {
+    const matches = input.match(new RegExp(regex, options));
+    console.log(matches); // Output: ["123", "456", "7890"]
+    return matches[0]
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+}
+
+/**
+ * Utility function for finding emails in strings
+ * @customfunction
+ * @param {string} input to search
+ * @returns {string} The matched pattern or error if not found.
+ */
+export function email(input) {
+  return regexsearch(input, '[\\w\\.-]+@[\\w\\.-]+\\.[a-zA-Z]{2,6}', "gmi");
+}
+
+/**
+ * Utility function for finding phones in strings
+ * @customfunction
+ * @param {string} input to search
+ * @returns {string} The matched pattern or error if not found.
+ */
+export function phone(input) {
+  return regexsearch(input, '(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}', "gmi");
+}
+
+/**
+ * Utility function for finding phones in strings
+ * @customfunction
+ * @param {string} input to search
+ * @returns {string} The matched pattern or error if not found.
+ */
+export function usphone(input) {
+  return regexsearch(input, '(\\+0?1\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}', "gmi");
+}
+
 /**
  * Simple Gen AI Function
  * @customfunction
@@ -62,6 +123,39 @@ export function genai(request, system_prompt, hint_text, max_tokens, temperature
   }
   //invocation.setResult("Loading")
 }
+
+/**
+ * Simple Gen AI Function for content extraction
+ * @customfunction
+ * @param {string} content Content to extract from
+ * @param {string} extract What you want to extract
+ */
+export function genaiextract(content, extract) {
+  try {
+    return genai(
+      "Extract " + extract + " from the following content: " + content, 
+      "You are a helpful, smart, kind, and efficient AI assistant. You always fulfill the user's requests to the best of your ability. You do not yap. ",
+      "Here is " + extract
+    );
+  } catch (e) {
+    return "ERROR: " + JSON.stringify(e.message);
+  }
+  //invocation.setResult("Loading")
+}
+
+/**
+ * Add two numbers
+ * @customfunction
+ * @param {number} first First number
+ * @param {number} second Second number
+ * @returns {number} The sum of the two numbers.
+
+export function add(first, second) {
+  console.log("TEST");
+  return first + second;
+}
+ */
+
 
 /**
  * Displays the current time once a second
